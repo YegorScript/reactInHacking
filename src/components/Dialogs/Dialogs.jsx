2aaf478f8c349cec25c4ece8a2a067dd6dd6bfd8
YegorScript <egorepir@gmail.com>
2020-12-08 23:13:36 +0600

    Add BLL

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,22 +16,29 @@ const Massage = (props) => {
   return <div className={s.mas}>{props.massage}</div>;
 };
 
+let dialogs = [
+  { id: 1, name: "Егор" },
+  { id: 2, name: "Ксения" },
+  { id: 3, name: "Дмитрий" },
+  { id: 4, name: "Тимур" },
+  { id: 5, name: "Эрик" },
+];
+let dialogsElement = dialogs.map((p) => <Dialog name={p.name} id={p.id} />);
+
+let massages = [
+  { id: 1, massage: "hello" },
+  { id: 2, massage: "Hello" },
+  { id: 3, massage: "Yo" },
+  { id: 4, massage: "Hacker?" },
+  { id: 5, massage: "Hi,Hacker" },
+];
+let massagesElement = massages.map((m) => <Massage massage={m.massage} />);
+
 const Dialogs = () => {
   return (
     <div className={s.dialogsAll}>
-      <div className={s.dialogsItem}>
-        <Dialog name="Егор" id="1" />
-        <Dialog name="Тимур" id="2" />
-        <Dialog name="Дмитрий" id="3" />
-        <Dialog name="Ксения" id="4" />
-        <Dialog name="Эрик" id="5" />
-      </div>
-      <div className={s.massages}>
-        <Massage massage="hello" />
-        <Massage massage="hello" />
-        <Massage massage="hello" />
-        <Massage massage="Hi, Hacker" />
-      </div>
+      <div className={s.dialogsItem}>{dialogsElement}</div>
+      <div className={s.massages}>{massagesElement}</div>
     </div>
   );
 };
--- src/components/Profile/MyPost/MyPost.jsx
+++ src/components/Profile/MyPost/MyPost.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import Post from "./Post/Post";
 import s from "./MyPost.module.css";
+let messages = [
+  { id: 1, message: "How are you?", like: 12 },
+  { id: 2, message: "It`s my first project", like: 48 },
+  { id: 3, message: "OH.My Hacker?", like: 12 },
+];
+let messagesElement = messages.map((m) => (
+  <Post message={m.message} id={m.id} like={m.like} />
+));
 const MyPost = () => {
   return (
     <div className={s.mp}>
@@ -12,10 +20,7 @@ const MyPost = () => {
         <button> add post</button>
       </div>
       <div>
-        <div className={s.post}>
-          <Post message="How are you?" like="0" />
-          <Post message="Its my first project!" like="48" />
-        </div>
+        <div className={s.post}>{messagesElement}</div>
       </div>
     </div>
   );
