59fc028638218ae349869edea4ede2b186068a55
YegorScript <egorepir@gmail.com>
2021-01-28 23:45:16 +0600

    get user api

--- src/components/Profile/MyPost/Post/Post.jsx
+++ src/components/Profile/MyPost/Post/Post.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import s from "./Post.module.css";
+import usersIcon from "../../../../assets/images/users.jpg";
 const Post = (props) => {
   return (
     <div className={s.items}>
-      <img src="https://st.depositphotos.com/1342206/1463/i/450/depositphotos_14634185-stock-photo-hackers-concept-digital-illustration.jpg" />
+      <img src={usersIcon} />
       <span className={s.item}>{props.message}</span>
       <div>like {props.like}</div>
     </div>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,25 @@
+import axios from "axios";
 import React from "react";
+import usersIcon from "../../assets/images/users.jpg";
 let Users = (props) => {
-  if (props.users.length === 0) {
-    props.SET_USERS([
-      {
-        id: 1,
-        followed: true,
-        fullName: "Dmitri",
-        status: "Good luck",
-        location: { country: "Russia", city: "Moscow" },
-      },
-      {
-        id: 2,
-        followed: false,
-        fullName: "Mark",
-        status: "Hi,my friends",
-        location: { country: "Ukraine", city: "Kiev" },
-      },
-      {
-        id: 3,
-        followed: true,
-        fullName: "Ann",
-        status: "Im the best",
-        location: { country: "Kazakhstan", city: "Almaty" },
-      },
-      {
-        id: 4,
-        followed: false,
-        fullName: "Yegor",
-        status: "OOO, my GOD",
-        location: { country: "Belarus", city: "Minsk" },
-      },
-    ]);
-  }
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          props.SET_USERS(response.data.items);
+        });
+    }
+  };
 
   return (
     <div>
+      <button onClick={getUsers}>getUsers</button>
       {props.users.map((u) => (
         <div key={u.id}>
           <span>
             <div>
-              <img src="#" />
+              <img src={u.photos.small != null ? u.photos.small : usersIcon} />
             </div>
             <div>
               {u.followed ? (
@@ -63,12 +43,12 @@ let Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.country}</div>
-              <div>{u.location.city}</div>
+              <div>{"u.location.country"}</div>
+              <div>{"u.location.city"}</div>
             </span>
           </span>
         </div>
